243be992a643a104009d31b26812826e2743ba8e
Manuel Rodríguez <manuel.rodriguezs@gmail.com>
2020-09-16 23:20:06 +0200

    Tarea periódica programada

--- src/main/java/com/manuelrodriguezs/SimpleScheduler.java
+++ src/main/java/com/manuelrodriguezs/SimpleScheduler.java
@@ -1,4 +1,18 @@
 package com.manuelrodriguezs;
 
+import lombok.extern.slf4j.Slf4j;
+
+import javax.ejb.Schedule;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+
+@Startup
+@Singleton
+@Slf4j
 public class SimpleScheduler {
+
+    @Schedule(hour = "*", minute = "*", second = "*/2", info = "Every 2 seconds timer")
+    public void scheduledTAsk() {
+        log.info("Hello scheduled world!");
+    }
 }
